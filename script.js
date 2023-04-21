3afb8d3e4e42b7761ee328116b69f927079bbe74
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-04-21 20:30:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get the button element
+var button = document.querySelector("input[type='button']");
+
+// Get the select element
+var select = document.getElementById("colorSelect");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+  // Get the selected option
+  var selectedOption = select.options[select.selectedIndex];
+
+  // Remove the selected option from the list
+  select.removeChild(selectedOption);
+});
